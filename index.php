b3c4908b99f3a4cc56b4b98106619bd933477a2c
yetnetbehailu <72866660+yetnetbehailu@users.noreply.github.com>
2026-02-17 14:08:33 +0000

    Install Bootstrap,Fontawesome & add fundamental folder structure

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="sv">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bokbunkern</title>
+</head>
+
+<body>
+    <header>
+
+    </header>
+
+    <main>
+
+    </main>
+
+    <footer>
+        
+    </footer>
+</body>
+
+</html>
