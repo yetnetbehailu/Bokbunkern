1f73a66111315978e377d4f75057305dd1e667ab
Ofelia Gärdefors <ofeliagardefors@gmail.com>
2026-03-18 11:44:12 +0000

    database/bokbunkern.sql

--- database/bokbunkern (1).sql
+++ database/bokbunkern (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Värd: mysql:3306
--- Tid vid skapande: 15 mars 2026 kl 20:18
+-- Tid vid skapande: 18 mars 2026 kl 11:42
 -- Serverversion: 8.0.45
 -- PHP-version: 8.3.30
 
@@ -24,15 +24,37 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Tabellstruktur `detail`
+--
+
+CREATE TABLE `detail` (
+  `comment_id` int UNSIGNED NOT NULL,
+  `user_id` int NOT NULL,
+  `comment` text COLLATE utf8mb3_unicode_ci NOT NULL,
+  `date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+
+--
+-- Dumpning av Data i tabell `detail`
+--
+
+INSERT INTO `detail` (`comment_id`, `user_id`, `comment`, `date`) VALUES
+(5, 2, 'oooo asså vilken bok!', '2026-03-16 18:19:03'),
+(6, 7, 'WOW vilken bok! Så bra!', '2026-03-17 13:45:58'),
+(26, 4, 'Hej! älskar denna boken.', '2026-03-18 09:54:16');
+
+-- --------------------------------------------------------
+
+--
 -- Tabellstruktur `users`
 --
 
 CREATE TABLE `users` (
   `user_id` int UNSIGNED NOT NULL,
-  `firstname` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `lastname` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `firstname` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `lastname` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `regdate` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -50,11 +72,18 @@ INSERT INTO `users` (`user_id`, `firstname`, `lastname`, `email`, `password`, `r
 (7, 'David', 'Nilsson', 'david@test.se', 'root', '2026-03-15 11:33:08'),
 (8, 'Emma', 'Karlsson', 'Emma@test.se', 'root', '2026-03-15 11:33:20'),
 (9, 'Oskar', 'Petterson', 'Oskar@test.se', 'root', '2026-03-15 11:33:32'),
-(10, 'Mina', 'Minasson', 'Mina@test.se', 'root', '2026-03-15 11:33:55');
+(10, 'Mina', 'Minasson', 'Mina@test.se', 'root', '2026-03-15 11:33:55'),
+(11, 'Ofelia', 'Gärdefors', 'ofelia@gmail.com', 'morot', '2026-03-16 11:15:34');
 
 --
 -- Index för dumpade tabeller
 --
+
+--
+-- Index för tabell `detail`
+--
+ALTER TABLE `detail`
+  ADD PRIMARY KEY (`comment_id`);
 
 --
 -- Index för tabell `users`
@@ -67,10 +96,16 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT för tabell `detail`
+--
+ALTER TABLE `detail`
+  MODIFY `comment_id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+
+--
 -- AUTO_INCREMENT för tabell `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `user_id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
