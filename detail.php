cdbdf834adffd9c2f477a3324a0b6078a57aaf8e
Ofelia Gärdefors <ofeliagardefors@gmail.com>
2026-03-23 13:22:08 +0000

    finnishing touches to comments-

--- detail.php
+++ detail.php
@@ -1,13 +1,9 @@
 <?php
-//include header
+//unclude header
 require_once 'assets/includes/header.php';
 ?>
 
-
-
-<main class="mt-2">
-
-
+<main class="mt-2 textc-cust">
     <!-- php sucsess massage -->
     <div class="container">
         <?php
@@ -50,18 +46,13 @@ require_once 'assets/includes/header.php';
     <!-- book description -->
     <div class="container p-4 text-bg-light rounded desc-box mx-auto">
         <div class="row align-items-center">
-
             <div class="d-flex justify-content-center">
                 <div class="col-3">
                     <img src="assets/images/acotar.jpg"
                         class="rounded w-75 border"
                         alt="En bild på bokomslaget till boken acotar.">
                 </div>
-
-
-
-
-                <div class="w-cust">
+                <div class="w-cust textc-cust">
                     <header class="fs-4 fw-bold">A court of thorns and roses</header>
                     <p>en romantisk fantasyroman och första delen i en populär bokserie.</p>
                     <p>Boken handlar om Feyre Archeron, en ung kvinna som lever i fattigdom med sin familj. När hon jagar i skogen dödar hon en varg för att kunna försörja dem. Men vargen visar sig egentligen vara en fae (en magisk varelse).
@@ -72,7 +63,6 @@ require_once 'assets/includes/header.php';
                     </div>
                 </div>
             </div>
-
         </div>
     </div>
 
@@ -83,17 +73,10 @@ require_once 'assets/includes/header.php';
             <div class="mx-5 mb-3 mt-5 row bg-cust1 rounded p-2">
                 <form action="detail.php" method="post">
                     <div class="row">
-
-
-
                         <div class="col">
                             <label for="comment" class="fs-4">Lägg till kommentar</label>
                             <textarea id="comment" name="comment" rows="2" cols="125" placeholder="Dela din åsikt..."></textarea>
                         </div>
-
-
-
-
                         <div class="col text-end align-items-center d-flex">
                             <button class="btnc" type="submit" name="register">
                                 Lägg upp <i class="mx-1 fa-solid fa-arrow-up"></i>
@@ -119,19 +102,15 @@ require_once 'assets/includes/header.php';
         </div>
     <?php endif; ?>
 
-    <!-- comments made -->
-
-
+    <!-- comments made (template)-->
     <?php
     //checks if there are any comments to show
     if ($stmt->rowCount() > 0):
     ?>
-
         <?php
         //fetches all comments and shows them on the page
         while ($row = $stmt->fetch()):
         ?>
-
             <div class="bg-cust2 p-3 rounded container mb-4">
                 <div class="row p-3 border m-2 bg-white border rounded">
                     <div class="col-1 fa-3x">
@@ -148,10 +127,9 @@ require_once 'assets/includes/header.php';
                                     <a href="edit.comment.php?edit=<?php echo $row['comment_id']; ?>" class="me-4 edt-btn">
                                         <i class="fa-solid fa-pen-to-square ic"></i> Redigera
                                     </a>
-                                    <form method="post" action="detail.php" style="display:inline;">
+                                    <form method="post" action="detail.php" style="display:inline;" name="remove">
                                         <input type="hidden" name="comment_id" value="<?php echo $row['comment_id']; ?>">
-
-                                        <a href="delete.comment.php?delete=<?php echo $row['comment_id']; ?>" name="remove" class="edt-btn">
+                                        <a href="delete.comment.php?delete=<?php echo $row['comment_id']; ?>" class="edt-btn">
                                             <i class="fa-solid fa-trash-can ic"></i> Radera
                                         </a>
                                     </form>
