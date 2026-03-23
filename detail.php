4da3587cdd30d0b2a24a634112ef4070b5b8582e
Ofelia Gärdefors <ofeliagardefors@gmail.com>
2026-03-23 09:55:46 +0000

    small changes to detail page and changed name on two files: about.php and faq.php

--- detail.php
+++ detail.php
@@ -48,21 +48,31 @@ require_once 'assets/includes/header.php';
     </div>
 
     <!-- book description -->
-    <div class="container p-4 text-bg-light rounded desc-box">
-        <div class="row align-items-start">
-            <div class="col-2">
-                <img src="assets/images/acotar.jpg"
-                    class="img-thumbnail"
-                    alt="En bild på bokomslaget till boken acotar.">
+    <div class="container p-4 text-bg-light rounded desc-box mx-auto">
+        <div class="row align-items-center">
+
+            <div class="d-flex justify-content-center">
+                <div class="col-3">
+                    <img src="assets/images/acotar.jpg"
+                        class="rounded w-75 border"
+                        alt="En bild på bokomslaget till boken acotar.">
+                </div>
+
+
+
+
+                <div class="w-cust">
+                    <header class="fs-4 fw-bold">A court of thorns and roses</header>
+                    <p>en romantisk fantasyroman och första delen i en populär bokserie.</p>
+                    <p>Boken handlar om Feyre Archeron, en ung kvinna som lever i fattigdom med sin familj. När hon jagar i skogen dödar hon en varg för att kunna försörja dem. Men vargen visar sig egentligen vara en fae (en magisk varelse).
+                        Som straff kommer en mäktig fae vid namn Tamlin och tar Feyre till sitt magiska rike Prythian. Hon tvingas bo i hans hov, men upptäcker snart att världen av fae är både farlig och fascinerande.</p>
+                    <p>Boken är löst inspirerad av sagan Skönheten och odjuret, men utvecklas till en större fantasyhistoria med politiska intriger och mörkare teman i senare böcker.</p>
+                    <div class="d-flex justify-content-end">
+                        <p><i class="fa-solid fa-star text-warning"></i><i class="fa-solid fa-star text-warning"></i><i class="fa-solid fa-star text-warning"></i><i class="fa-regular fa-star"></i><i class="fa-regular fa-star"></i></p>
+                    </div>
+                </div>
             </div>
-            <div class="col-10">
-                <header class="fs-4 fw-bold">A court of thorns and roses</header>
-                <p>en romantisk fantasyroman och första delen i en populär bokserie.</p>
-                <p>Boken handlar om Feyre Archeron, en ung kvinna som lever i fattigdom med sin familj. När hon jagar i skogen dödar hon en varg för att kunna försörja dem. Men vargen visar sig egentligen vara en fae (en magisk varelse).
-                    Som straff kommer en mäktig fae vid namn Tamlin och tar Feyre till sitt magiska rike Prythian. Hon tvingas bo i hans hov, men upptäcker snart att världen av fae är både farlig och fascinerande.</p>
-                <p>Boken är löst inspirerad av sagan Skönheten och odjuret, men utvecklas till en större fantasyhistoria med politiska intriger och mörkare teman i senare böcker.</p>
-                <p><i class="fa-solid fa-star text-warning"></i><i class="fa-solid fa-star text-warning"></i><i class="fa-solid fa-star text-warning"></i><i class="fa-regular fa-star"></i><i class="fa-regular fa-star"></i></p>
-            </div>
+
         </div>
     </div>
 
@@ -73,11 +83,18 @@ require_once 'assets/includes/header.php';
             <div class="mx-5 mb-3 mt-5 row bg-cust1 rounded p-2">
                 <form action="detail.php" method="post">
                     <div class="row">
-                        <div class="col-10">
+
+
+
+                        <div class="col">
                             <label for="comment" class="fs-4">Lägg till kommentar</label>
-                            <input class="form-control" type="text" placeholder="Dela din åsikt..." id="comment" name="comment" />
+                            <textarea id="comment" name="comment" rows="2" cols="125" placeholder="Dela din åsikt..."></textarea>
                         </div>
-                        <div class="col text-end align-items-end d-flex">
+
+
+
+
+                        <div class="col text-end align-items-center d-flex">
                             <button class="btnc" type="submit" name="register">
                                 Lägg upp <i class="mx-1 fa-solid fa-arrow-up"></i>
                             </button>
